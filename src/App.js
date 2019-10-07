7303a2fa87f45a1d1b293e748dc02393f641c3ea
Steven Sun <stevensun1026@gmail.com>
2019-10-07 00:19:40 -0700

    fixed first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, welcome to my website.
         </p>
         <a
           className="App-link"
@@ -16,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a link.
         </a>
       </header>
     </div>

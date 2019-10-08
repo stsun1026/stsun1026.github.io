343162a1a412ae0e4274872478d90a4edb499c4f
Steven Sun <stevensun1026@gmail.com>
2019-10-08 01:08:39 -0700

    integrated router into top bar

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
 import React from 'react';
 import './App.css';
-import pic from './assets/steven-n-the-bees-cropped.jpg';
-import FormattedImage from './components/FormattedImage';
+
 import SamplePage from './pages/samplePage';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { withRouter } from 'react-router-dom'
+
 
 function App() {
+  const Button = withRouter(({ history }) => (
+    <button
+      type='button'
+      onClick={() => { history.push('/l2') }}
+    >
+      Click Me!
+    </button>
+  ));
+
+    const sp = SamplePage({content: "hello"});
   return (
     <div className="App">
+            <Router>
+
       <header className="App-header">
-      <Router>
-      <div>
         <div>
               <Link to="/">Home</Link>
               <Link to="/l1">Link2</Link>
-              <Link to="/l2">Link3</Link>
+              <Button/>
         </div>
-        <FormattedImage image = {pic} sizeRatio = "0.98"/>  
-        <p>
-          Hello, welcome to my website.
-        </p>
-        <a
-          className="App-link"
-          href="https://media.giphy.com/media/fsQQb3KSaBxnuFaXOk/giphy.gif"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is an external link.
-        </a>
         <Switch>
           <Route path="/l1">
-            <SamplePage content={"This is the content of link 1"}/>
+            <SamplePage content={"This is some text and here is some more text"}/>
           </Route>
-          <Route path="/l2">
-            <SamplePage content={"This is the content of link 2"}/>
-          </Route>
-          
+          <Route path="/l2" component={() => sp}/>
+            {/* <SamplePage content={"This is the content of link 2"}/> */}
+
           <Route path = "/">
-            <SamplePage content="This is the content of the home page."/>
+            <SamplePage/>
           </Route>
-
         </Switch>
-      </div>
+    </header>
     </Router>
-      </header>
-
+    
     </div>
-
   );
 }
-//this is a git demo
 export default App;
--- src/pages/samplePage.js
+++ src/pages/samplePage.js
@@ -1,6 +1,32 @@
 import React from 'react';
-const SamplePage = ({content}) =>{
-  return(<p>{content}</p>);
+import pic from '../assets/steven-n-the-bees-cropped.jpg';
+import FormattedImage from '../components/FormattedImage';
+
+
+const SamplePage = ({
+  content =  <p>Hello, welcome to my website. </p>
+}) =>{
+
+  return(<div>
+        <FormattedImage image = {pic} sizeRatio = "0.98"/>  
+        <div>
+          {content}
+        </div>
+        <a
+          className="App-link"
+          href="https://media.giphy.com/media/fsQQb3KSaBxnuFaXOk/giphy.gif"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is an external link.
+        </a>
+        
+
+    </div>);
 }
 
 export default SamplePage;
+
+
+
+

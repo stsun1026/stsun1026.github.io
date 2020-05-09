5d4d909ab094978bdd3c0eecaf3dd4d27508e691
Steven Sun <stevensun1026@gmail.com>
2020-05-08 20:45:08 -0600

    Fix: changed name of game on HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -13,7 +13,7 @@ const HomePage = () => {
       <div className={ styles.welcome } style={{ textAlign: isShrunk ? "center" : "left", paddingLeft: isShrunk ? 0 : 20 }}>welcome</div>
       <div className={ styles.menuContainer } style={{ width: isShrunk ? width : MAX_SIZE }}>
         { linkify({ component:<LargeTile text="about + contact"/>, route:"/l1" }) }
-        { linkify({ component:<LargeTile text="clear the pipe"/>, route:"/clear-the-tree" }) }
+        { linkify({ component:<LargeTile text="clear the tree"/>, route:"/clear-the-tree" }) }
         { linkify({ component:<LargeTile text="another thing"/>, route:"/l1" }) }
         { linkify({ component:<LargeTile text="thing 3"/>, route:"/l1" }) }
       </div>

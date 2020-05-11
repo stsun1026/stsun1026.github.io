ca97a62e2bc074e2a64f883a192a1499ae5899ac
Steven Sun <stevensun1026@gmail.com>
2020-05-11 09:08:21 -0600

    Created topbar component

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./Topbar.module.css";
+import { linkify } from "../../services/RoutingService"
+import Button from "../Button/Button";
+const Topbar = ({
+  children, 
+}) => {
+  return (
+    <div className={ styles.container }>
+      <div className={ styles.leading }>
+      { linkify({ component: <Button label="HOME"/>, route: "/" }) }
+      </div>
+      <div className={ styles.trailing }>
+        { linkify({ component: <Button label="EXPERIENCE"/>, route: "/" }) }
+        { linkify({ component: <Button label="PROJECTS"/>, route: "/" }) }
+        { linkify({ component: <Button label="PHOTOS"/>, route: "/" }) }
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;

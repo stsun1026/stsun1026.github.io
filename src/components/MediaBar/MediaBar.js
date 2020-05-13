0b7937a5221ceeea151a5367d68da334a2a732a0
Steven Sun <stevensun1026@gmail.com>
2020-05-13 08:33:54 -0600

    Revert media bar

--- src/components/MediaBar/MediaBar.js
+++ src/components/MediaBar/MediaBar.js
@@ -9,6 +9,10 @@ const MediaBar = ({
     <div 
       className={ styles.container }
     >
+      <a className={ styles.link } href="https://github.com/stsun1026" target="_blank"><FaGithub/></a>
+      <a className={ styles.link } href="https://www.linkedin.com/in/steven-sun-195542145/" target="_blank"><FaLinkedin/></a>
+      <a className={ styles.link } href="mailto:stevensun1026@gmail.com" target="_blank"><FaEnvelope/></a>
+      <a className={ styles.link } href="https://twitter.com/stsun16" target="_blank"><FaTwitter/></a>
     </div>
   );
 }

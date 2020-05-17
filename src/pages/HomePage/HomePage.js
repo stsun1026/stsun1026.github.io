dbb9b478327d59607820176eb965a077ffcbc1cf
Steven Sun <stevensun1026@gmail.com>
2020-05-17 11:43:22 -0600

    Created responsive mobile view for home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,30 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import Topbar from "../../components/Topbar/Topbar";
+import TopbarDropdown from "../../components/TopbarDropdown/TopbarDropdown";
 import MediaBar from "../../components/MediaBar/MediaBar";
 import useWindowDimensions from "../../services/WindowDimensions";
 import headshot from "../../assets/photo-steven-sun.jfif";
 import styles from "./HomePage.module.css";
 
-const MAX_SIZE = 680;
+const SHRINK_PICTURE_WIDTH = 1050;
+const MOBILE_VIEW_MAX_WIDTH = 810;
 const HomePage = () => {
   const { height, width } = useWindowDimensions();
-  const isShrunk = width < MAX_SIZE;
+  const isMobileView = width < MOBILE_VIEW_MAX_WIDTH;
+  const getPicWidth = () => {
+    if(isMobileView) {
+      return 350;
+    } else if(width < SHRINK_PICTURE_WIDTH) {
+      return 450 - SHRINK_PICTURE_WIDTH + width
+    } else {
+      return 450;
+    }
+  }
+
   return(
-    <div style={{ width: width, height: height}}>
-      <Topbar/>
-      <div className={ styles.contentContainer } style={{ height: Math.max(height - 42,   510)}}>
-        <div style={{ paddingTop: 50 }}>
-          <img src={ headshot } className={ styles.photo }></img>
-          <div className={ styles.content }>
-            <div className={ styles.nameText }>Steven Sun</div>
-            <p style={{ fontSize: 20 }}>
-              Software developer making the transition from intern to full time, 
-              passionate about full stack web development, and using this website 
-              to learn frontend and design.
-            </p>
-            <div className={ styles.mediaBar }>
-              <MediaBar />
-            </div>
+    <div className={ styles.container } style={{ height: height, overflowY: isMobileView ? "scroll" : "hidden" }}>
+      { isMobileView ? <TopbarDropdown/> : <Topbar/> }
+      <div 
+        className={ styles.contentContainer }
+        style={{ 
+          height: Math.max(height - 42, 510),
+          paddingTop: isMobileView ? 0 : 50
+        }}
+      >
+        <div style={{ display: isMobileView ? "block" : "inline" }}>
+        <img src={ headshot }
+          className={ styles.photo }
+          style={{ width: getPicWidth(), marginTop: (450 - getPicWidth) / 4 }}
+        />
+        </div>
+        <div 
+          className={ styles.content }
+          style={{ paddingLeft: isMobileView ? 0 : 150, paddingTop: isMobileView ? 20 : 50 }}
+        >
+          <div className={ styles.nameText } style={{ fontSize: isMobileView ? 55 : 65 }}>Steven Sun</div>
+          <p style={{ fontSize: 20 }}>
+            Software developer making the transition from intern to full time, 
+            passionate about full stack web development, and using this website 
+            to learn frontend and design.
+          </p>
+          <div className={ styles.mediaBar }>
+            <MediaBar />
           </div>
         </div>
       </div>

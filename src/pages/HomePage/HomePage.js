7f4d27e871464addc15f363946cb4d9f1def011a
Steven Sun <stevensun1026@gmail.com>
2020-05-19 09:31:07 -0600

    Refactored Home Page, layout is now vertical

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Topbar from "../../components/Topbar/Topbar";
 import TopbarDropdown from "../../components/TopbarDropdown/TopbarDropdown";
 import MediaBar from "../../components/MediaBar/MediaBar";
@@ -6,55 +6,24 @@ import useWindowDimensions from "../../services/WindowDimensions";
 import headshot from "../../assets/photo-steven-sun.jfif";
 import styles from "./HomePage.module.css";
 
-const SHRINK_PICTURE_WIDTH = 1050;
 const MOBILE_VIEW_MAX_WIDTH = 810;
 const HomePage = () => {
   const { height, width } = useWindowDimensions();
   const isMobileView = width < MOBILE_VIEW_MAX_WIDTH;
-  const getPicWidth = () => {
-    if(isMobileView) {
-      return 350;
-    } else if(width < SHRINK_PICTURE_WIDTH) {
-      return 450 - SHRINK_PICTURE_WIDTH + width
-    } else {
-      return 450;
-    }
-  }
 
   return(
-    <div className={ styles.container } 
-      style={{
-        // height: isMobileView ? "auto" : height,
-        overflowY: isMobileView ? "auto" : "hidden" 
-      }}
-    >
+    <div className={ styles.container }>
       { isMobileView ? <TopbarDropdown/> : <Topbar/> }
-      <div 
-        className={ styles.contentContainer }
-        style={{ 
-          height: isMobileView ? "auto" : Math.max(height - 95, 450),
-          paddingTop: isMobileView ? 20 : 50
-        }}
-      >
-        <div style={{ display: isMobileView ? "block" : "inline" }}>
-          <img src={ headshot }
-            className={ styles.photo }
-            style={{ width: getPicWidth(), marginTop: isMobileView ? 0 : (450 - getPicWidth) / 4 }}
-          />
-        </div>
-        <div 
-          className={ styles.content }
-          style={{ paddingLeft: isMobileView ? 0 : 150, paddingTop: isMobileView ? 20 : 50 }}
-        >
-          <div className={ styles.nameText } style={{ fontSize: isMobileView ? 55 : 65 }}>Steven Sun</div>
-          <p style={{ fontSize: 20 }}>
+      <div className={ styles.contentContainer } style={{ height: height - 45 }}>
+        <div style={{ maxWidth: 750, paddingLeft: 20, paddingRight: 20}}>
+          <img src={ headshot } className={ styles.photo }/>
+          <div style={{ fontSize: isMobileView ? 55 : 65 }}>Steven Sun</div>
+          <div className={ styles.intro }>
             Software developer making the transition from intern to full time, 
             passionate about full stack web development, and using this website 
             to learn frontend and design.
-          </p>
-          <div className={ styles.mediaBar }>
-            <MediaBar />
           </div>
+          <div className={ styles.mediaBar }><MediaBar/></div>
         </div>
       </div>
     </div>

5ca75567684cd4b19b8a24c6ca4b6e4b9443242d
Steven Sun <stevensun1026@gmail.com>
2020-05-22 23:38:23 -0600

    Added fade in animation to content on home page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState  } from 'react';
 import Topbar from "../../components/Topbar/Topbar";
 import TopbarDropdown from "../../components/TopbarDropdown/TopbarDropdown";
 import MediaBar from "../../components/MediaBar/MediaBar";
@@ -9,13 +9,18 @@ import styles from "./HomePage.module.css";
 const MOBILE_VIEW_MAX_WIDTH = 600;
 const HomePage = () => {
   const { height, width } = useWindowDimensions();
+  const [isRendered, setIsRendered] = useState(false);
   const isMobileView = width < MOBILE_VIEW_MAX_WIDTH;
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
 
   return(
     <div className={ styles.container }>
       { isMobileView ? <TopbarDropdown/> : <Topbar/> }
       <div className={ styles.contentContainer } style={{ height: height - 45 }}>
-        <div style={{ maxWidth: 750, paddingLeft: 20, paddingRight: 20}}>
+        <div className={ isRendered ? styles.content : styles.contentBeforeAnimation }>
           <img src={ headshot } className={ styles.photo }/>
           <div style={{ fontSize: isMobileView ? 55 : 65 }}>Steven Sun</div>
           <div className={ styles.intro }>
